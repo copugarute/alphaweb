098488143905d6143312f8976f2da48cc8087648
cpuga <copugarute@gmail.com>
2024-01-23 20:03:12 -0300

    actualizando base de datos

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,13 @@ cp index.html 404.html
 # git instrucction
 
 git init 
-git add .
+git add -A
 git commit -m "deploy"
 
 
 # Para desplegar nuestra pagina en gh-pages  https://<USERNAME>.github.io/<REPO>
 # !importante ! reemplazar por su propio git la siguiente linea donde corresponda
 
-git push -f origin https://github.com/copugarute/alphaweb.git main
+git push -f https://github.com/copugarute/alphaweb.git main
 
 cd -
